84bd8558ff4bff8b0a541d9639d3d8d90004a864
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-11 19:28:49 -0500

    fix favicon error

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,6 @@ const FILES_TO_CACHE = [
 	"/",
 	"/index.html",
 	"db.js",
-	"favicon.ico",
 	"index.js",
 	"manifest.webmanifest",
 	"service-worker.js",
@@ -17,27 +16,11 @@ const DATA_CACHE_NAME = "data-cache-v1";
 // install
 self.addEventListener("install", function (evt) {
 	evt.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then((cache) => {
-				console.log("Your files were pre-cached successfully!");
-				cache
-					.addAll(FILES_TO_CACHE)
-					.then((result) => {
-						// debugger;
-						console.log("result of add all", result);
-					})
-					.catch((err) => {
-						// debugger;
-						console.log("Add all error: ", err);
-					});
-			})
-			.catch((err) => {
-				console.log(err);
-			})
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log("Your files were pre-cached successfully!");
+			return cache.addAll(FILES_TO_CACHE);
+		})
 	);
-
-	self.skipWaiting();
 });
 
 // activate
